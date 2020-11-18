4fb5278020224a6d5f9273a8beeb1e3d1c735ee6
JimmyPoor <kissnana3@163.com>
2020-11-18 17:18:29 +0800

    add video plugin

--- shserc_site/src/main.js
+++ shserc_site/src/main.js
@@ -9,11 +9,18 @@ import '@/static/elementUI.js'
 import '@/static/validator.js'
 import '@/assets/css/global.css'
 import { initDic } from '@/app/utils/dictHelper'
+import VideoPlayer  from 'vue-video-player'
 
+/** initial for global store */
 initDic(store);
 
 Vue.prototype.instance = instance_;
 Vue.config.productionTip = false
+
+/** initial for video player */
+Vue.use(VideoPlayer)
+require('video.js/dist/video-js.css')
+require('vue-video-player/src/custom-theme.css')
 
 /* eslint-disable no-new */
 new Vue({
--- shserc_site/src/views/admin/resource/detail.vue
+++ shserc_site/src/views/admin/resource/detail.vue
@@ -7,9 +7,16 @@
     <el-container>
       <el-main>
         <div class="grid text_align_left">
-          <div class="grid-item">
-            <div>
-              <img class="cover" v-if=" resource.resourceTypeId != appEnum.resourceVideoTypeKey" :src="resource.coverPath" />
+          <div class="grid-item" >
+             <div class="cover" v-if="resource.resourceTypeId == appEnum.resourceVideoTypeKey">
+              <img :src="resource.coverPath" />
+            </div> 
+            <div class="video" v-else>
+              <video-player  class="video-player vjs-custom-skin"
+                   ref="videoPlayer"
+                   :playsinline="true"
+                  :options="playerOptions"
+                ></video-player>
             </div>
           </div>
           <div class="grid-item font14">
@@ -111,7 +118,31 @@ export default {
   data() {
     return {
       resource: resourceModel,
-      appEnum:appEnum
+      appEnum:appEnum,
+
+      playerOptions : {
+                    playbackRates : [ 0.5, 1.0, 1.5, 2.0 ], //可选择的播放速度
+                    autoplay : false, //如果true,浏览器准备好时开始回放。
+                    muted : false, // 默认情况下将会消除任何音频。
+                    loop : false, // 视频一结束就重新开始。
+                    preload : 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+                    language : 'zh-CN',
+                    aspectRatio : '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+                    fluid : true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+                    sources : [ {
+                        type : "",
+                        src : 'http://shlll.s.wcsapi.biz.matocloud.com/upload201909261929354303.mp4'//url地址
+                    } ],
+                    poster : "", //你的封面地址
+                    // width: document.documentElement.clientWidth,
+                    notSupportedMessage : '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+                    controlBar : {
+                        timeDivider : true,//当前时间和持续时间的分隔符
+                        durationDisplay : true,//显示持续时间
+                        remainingTimeDisplay : false,//是否显示剩余时间功能
+                        fullscreenToggle : true  //全屏按钮
+                    }
+                }
     };
   },
   computed: {
@@ -134,6 +165,10 @@ export default {
 </script>
 
 <style scoped>
+.video{
+  width:500px;
+}
+
 .grid {
   width: 100%;
   height: auto;
@@ -152,7 +187,7 @@ export default {
   margin: 20px;
 }
 
-.cover {
+.cover img {
   width: 100%;
   max-width: 600px;
   transition: all 0.5s;
